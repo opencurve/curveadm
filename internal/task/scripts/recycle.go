4c81f019ba4280a6cc12ccadadc4a0a47d4675b4
Wine93 <wine93.info@gmail.com>
2022-02-18 15:02:44 +0800

    bugfix(curvebs): fixed recycle chunk file with clean suffix.

--- internal/task/scripts/recycle.go
+++ internal/task/scripts/recycle.go
@@ -31,6 +31,7 @@ g_source=$1
 g_dest=$2
 g_size=$3
 chunkid=$(ls -vr ${g_dest} | head -n 1)
+chunkid=${chunkid%.clean}
 for file in $(find $g_source -type f -size ${g_size}c -printf '%p\n'); do
     chunkid=$((chunkid+1))
     mv $file $g_dest/$chunkid
