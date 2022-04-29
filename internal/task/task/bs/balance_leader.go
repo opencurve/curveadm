265b29582f13a80dfe9ab683c8c737249d8d13fa
YhhHaa <2168929065@qq.com>
2022-04-29 15:02:34 +0800

    feature: balance leader rapidly when executing deploy function.

---
+++ internal/task/task/bs/balance_leader.go
@@ -0,0 +1,62 @@
+/*
+ *  Copyright (c) 2021 NetEase Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+/*
+ * Project: CurveAdm
+ * Created Date: 2022-04-22
+ * Author: Ye Tao (YhhHaa)
+ */
+
+package bs
+
+import (
+	"fmt"
+
+	"github.com/opencurve/curveadm/cli/cli"
+	"github.com/opencurve/curveadm/internal/configure/topology"
+	"github.com/opencurve/curveadm/internal/task/step"
+	"github.com/opencurve/curveadm/internal/task/task"
+	tui "github.com/opencurve/curveadm/internal/tui/common"
+)
+
+// balance leader rapidly.
+func NewBalanceTask(curveadm *cli.CurveAdm, dc *topology.DeployConfig) (*task.Task, error) {
+	// get id of service and container.
+	serviceId := curveadm.GetServiceId(dc.GetId())
+	containerId, err := curveadm.Storage().GetContainerId(serviceId)
+	if err != nil {
+		return nil, err
+	} else if containerId == "" {
+		return nil, fmt.Errorf("service(id=%s) not found", serviceId)
+	}
+
+	// define a task.
+	name := "Balance Leader"
+	subname := fmt.Sprintf("host=%s role=%s containerId=%s",
+		dc.GetHost(), dc.GetRole(), tui.TrimContainerId(containerId))
+	t := task.NewTask(name, subname, dc.GetSSHConfig())
+
+	// balance leader.
+	t.AddStep(&step.ContainerExec{
+		ContainerId:   &containerId,
+		Command:       "curve_ops_tool rapid-leader-schedule",
+		ExecWithSudo:  true,
+		ExecInLocal:   false,
+		ExecSudoAlias: curveadm.SudoAlias(),
+	})
+
+	return t, nil
+}
