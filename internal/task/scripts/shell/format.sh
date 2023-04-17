06d2e69bdc057ea49d78a421f204682715728b23
Wine93 <wine93.info@gmail.com>
2023-04-17 14:27:20 +0800

    Improve: use embed module to embedded static shell scripts.
    Signed-off-by: Wine93 <wine93.info@gmail.com>

---
+++ internal/task/scripts/shell/format.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Created Date: 2021-12-27
+# Author: Jingli Chen (Wine93)
+
+
+binary=$1
+percent=$2
+chunkfile_size=$3
+chunkfile_pool_dir=$4
+chunkfile_pool_meta_path=$5
+
+mkdir -p $chunkfile_pool_dir
+$binary \
+  -allocatePercent=$percent \
+  -fileSize=$chunkfile_size \
+  -filePoolDir=$chunkfile_pool_dir \
+  -filePoolMetaPath=$chunkfile_pool_meta_path \
+  -fileSystemPath=$chunkfile_pool_dir
